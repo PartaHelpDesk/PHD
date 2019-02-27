7bad39abe854a3c710dbb71282f614b4b17d9688
PartaHelpDesk <PartaHelpDesk@gmail.com>
2019-02-27 23:22:16 +0000

    updated

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,12 @@ from app import app
 @app.route('/index')
 def index():
     return "Hello, World!"
-    
+
+@app.route('/login')
+def login():
+    return "This will be the PHD login url"
+
+@app.route('/dashboard')
+def dashboard():
+    return "This will be a customized dahboard"
+
