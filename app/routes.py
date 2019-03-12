b76db2a7bf662d63c4ea6ca7d2d2c2bf06e8f5e1
bad-data <minusben@gmail.com>
2019-03-12 10:29:55 -0400

    updated routes

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,22 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+	return '''
+<html>
+<head>
+<title>PartaHelpDesk 1.0.0</title>
+</head>
+<body>
+
+<h1 >PHD Login Page</h1>
+ <form>
+  Email:<br>
+  <input type="text" name="Email"><br>
+  Password:<br>
+  <input type="text" name="Password"> <br>
+  <input type="submit" value="Submit">
+</form> 
+</body>
+</html>
+'''
+
