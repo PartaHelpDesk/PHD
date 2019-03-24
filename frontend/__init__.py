abf94451cc603e8a973cd7fe5542a49e14964f03
hliu32 <hliu32@kent.edu>
2019-03-24 18:19:15 +0800

    Subordinate to the main branch

--- frontend/__init__.py
+++ frontend/__init__.py
@@ -1,16 +1,8 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 from flask_mail import Mail
-from config import config
-
 
 frontend = Flask(__name__)
-db = SQLAlchemy(frontend)
 mail = Mail(frontend)
-login_manager = LoginManager(frontend)
-login_manager.login_message = 'Please log in before you do anything else.'
-login_manager.session_protection = 'strong'
-login_manager.login_view = 'user.login'
 
 from frontend import routes, main_routes, user_routes
+from frontend import config
---
+++ frontend/config.py
@@ -0,0 +1,15 @@
+from frontend import frontend
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_mail import Mail
+from config import config
+
+db = SQLAlchemy(frontend)
+mail = Mail(frontend)
+login_manager = LoginManager(frontend)
+login_manager.login_message = 'Please log in before you do anything else.'
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'user.login'
+
+# load config
+frontend.config.from_object(config.get('dev'))
