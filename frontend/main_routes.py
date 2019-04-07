3e42192a3141449e4a0d15889910e36effd8b3a0
hliu32 <hliu32@kent.edu>
2019-04-07 15:16:22 +0800

    finishi view ticket page

--- frontend/main_routes.py
+++ frontend/main_routes.py
@@ -1,5 +1,5 @@
 from . import frontend
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, abort
 from flask_login import login_required
 from .utils import *
 
@@ -9,10 +9,20 @@ from .utils import *
 def dashboard_2():
     my_tickets = get_my_tickets()
     tickets_queue = get_tickets_queue()
-    return render_template("dashboard.html", my_tickets=my_tickets, tickets_queue=tickets_queue)
+    return render_template("ticket_queue.html", my_tickets=my_tickets, tickets_queue=tickets_queue)
 
 
 @frontend.route('/hliu32')
 @login_required
 def index_2():
     return redirect(url_for('login_2'))
+
+
+@frontend.route("/hliu32/view_ticket/<int:ticket_id>")
+@login_required
+def view_ticket(ticket_id):
+    ticket = Ticket.query.get(ticket_id)
+    if not ticket:
+        abort(404)
+
+    return render_template("view_ticket.html", t=ticket)
