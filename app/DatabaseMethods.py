659cd43ba3ce0bca63e13daa40d06031bd40feec
PartaHelpDesk <PartaHelpDesk@gmail.com>
2019-04-13 15:57:04 -0400

    db cleanup

--- app/DatabaseMethods.py
+++ app/DatabaseMethods.py
@@ -1,6 +1,5 @@
 
 from app import Datatable, DataRow #DEBUG
-
 import pyodbc
 from array import *
 
@@ -36,7 +35,7 @@ class DatabaseMethods:
 
 
     def GetValue(self, sqlstring, params):
-        cursor = DatabaseMethods.ExecuteSql(self, sqlstring, params, True)
+        cursor = self.ExecuteSql( sqlstring, params, True)
         result = cursor.fetchone()
 
         cursor.close()
@@ -49,7 +48,7 @@ class DatabaseMethods:
 
 
     def GetDataTable(self, sqlstring, params):
-        cursor = DatabaseMethods.ExecuteSql(self, sqlstring, params, True)
+        cursor = self.ExecuteSql( sqlstring, params, True)
 
         column_names = [column[0] for column in cursor.description]
         column_count = len(column_names)
@@ -71,7 +70,7 @@ class DatabaseMethods:
 
     def GetITEmails(self):
         sql = "SELECT Email from Users WHERE [Level] in (2,3)"
-        dt = DatabaseMethods.GetDataTable(self, sql, None)
+        dt = self.GetDataTable( sql, None)
         index = 0
         list_of_emails = []
         while index != dt.get_Size():
@@ -82,34 +81,34 @@ class DatabaseMethods:
 
     def GetUserID(self, user_name):
         sql = "SELECT UserID FROM USERS WHERE Username = ?"
-        id = DatabaseMethods.GetValue(self, sql, user_name)
+        id = self.GetValue(sql, user_name)
         return id
 
     def GetUserAccountInfo(self, user_id):
         #Gets all user info
         sql = "SELECT * FROM Users WHERE UserID = ?"
-        return self.GetDataTable(self, sql, user_id)
+        return self.GetDataTable(sql, user_id)
 
     def GetTicketInfo(self, ticket_id):
         #Gets ticket infor for one ticket
         sql = "SELECT * FROM Tickets WHERE TicketID = ?"
-        return self.GetDataTable(self, sql, ticket_id)
+        return self.GetDataTable( sql, ticket_id)
 
     def GetAllActiveTickets(self):
         #Gets all active tickets (admin/IT)
         sql = "SELECT * FROM Tickets WHERE [Status] <> 'Closed'"
-        return self.GetDataTable(self, sql, None)
+        return self.GetDataTable(sql, None)
 
     def GetAllUserTickets(self, user_id):
         #Dashboard Tickets related to user
         sql = "SELECT * FROM Tickets WHERE CreatedUserID = ? "
-        return self.GetDataTable(self, sql, user_id)
+        return self.GetDataTable( sql, user_id)
 
     def CreateTicket(self, title, category, user_id, status, department, description):
         #Creates a ticket
         sql = "INSERT INTO Tickets (Title, Category, CreatedUserID, [Status], Department, [Description]) "
         sql = sql + "VALUES ( ?, ?, ?, ?, ?, ?) "
-        DatabaseMethods.ExecuteSql(self, sql, (title, category, user_id, status, department, description),False)
+        self.ExecuteSql(sql, (title, category, user_id, status, department, description),False)
 
     def UpdateTicket(self, user_id, ticket_id, title, category, status, department, description):
             #Updates the ticket in ticket table and updates ticket history
@@ -122,7 +121,7 @@ class DatabaseMethods:
             #Get current ticket info
             sql = "SELECT * FROM Tickets WHERE TicketID = ?"
 
-            dt = DatabaseMethods.GetDataTable(self, sql, (ticket_id))
+            dt = self.GetDataTable(sql, (ticket_id))
             dr = dt.GetRow(0)
 
             old_title = dr.GetColumnValue("Title")
@@ -161,24 +160,24 @@ class DatabaseMethods:
                 sql = sql + " SET Title = ?, Category = ?, [Status] = ?, Department = ?, [Description] = ? "
                 sql = sql + " WHERE TicketID = ?"
                 
-                DatabaseMethods.ExecuteSql(self, sql, (title, category, status, department, description, ticket_id), False)
+                self.ExecuteSql(sql, (title, category, status, department, description, ticket_id), False)
 
 
                 #update ticket history
                 sql = "INSERT INTO TicketHistory (TicketID, Title, Category, [Status], Department, [Description], UserID)"
                 sql = sql + "VALUES ( ?, ?, ?, ?, ?, ?, ?)"
                 
-                DatabaseMethods.ExecuteSql(self, sql, (ticket_id, update_title, update_category, update_status, update_department, update_description ,user_id), False)
+                self.ExecuteSql(sql, (ticket_id, update_title, update_category, update_status, update_department, update_description ,user_id), False)
 
-        def GetCategories(self):
-            sql = "SELECT * FROM Categories"
-            return DatabaseMethods.GetDataTable(self, sql, None)
+    def GetCategories(self):
+        sql = "SELECT * FROM Categories"
+        return self.GetDataTable(sql, None)
 
-        def GetDepartments(self):
-            sql = "SELECT * FROM Departments"
-            return DatabaseMethods.GetDataTable(self, sql, None)
+    def GetDepartments(self):
+        sql = "SELECT * FROM Departments"
+        return self.GetDataTable(sql, None)
 
-        def GetStatuses(self):
-            sql = "SELECT * FROM Status"
-            return DatabaseMethods.GetDataTable(self, sql, None)
+    def GetStatuses(self):
+        sql = "SELECT * FROM Status"
+        return self.GetDataTable(sql, None)
         
