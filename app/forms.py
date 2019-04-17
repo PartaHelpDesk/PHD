1e97d81b793c428396eea18a2eb95025eabe9dd9
jsomwaru <jacobsomwaru@gmail.com>
2019-04-17 17:56:07 -0400

    fixed create ticket page

--- app/forms.py
+++ app/forms.py
@@ -10,18 +10,9 @@ class EmailForm(FlaskForm):
     submit = SubmitField('Submit')
 
 class TicketForm(FlaskForm):
-	
-	catagories = []
-	
-	def __init__(self):
-		dbm = dm.DatabaseMethods()
-		categories_dt = dbm.GetCategories()
-		for category in categories_dt.data_rows:
-			self.catagories.append(category)
-
 	department = StringField('Department:', validators=[DataRequired()])
 	ticketTitle = StringField('Ticket Title: ', validators=[DataRequired()])
 	ticketDescription = StringField('Description of problem: ', validators=[DataRequired()])
-	ticketCategory = SelectField('Category: ', choices=catagories, validators=[DataRequired()])
+	ticketCategory = SelectField('Category: ', choices=[], validators=[DataRequired()])
 	#ticketStatus = StringField('Status: ', validators=[DataRequired()])
 	submit = SubmitField('Send Ticket')
